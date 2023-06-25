1ab6a7c36e590f6bd92f0e05d328303ff80c6e86
Guanmin Liao <77824905+mingmoth@users.noreply.github.com>
2023-06-25 17:17:25 +0800

    [FireBase] 串接 (#1)
    * feat: add firebase index.js for initialize & setting config
    
    * feat: import firebase & vuefire plugin into app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { VueFire } from 'vuefire'
+import { firebaseApp } from './firebase'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app  = createApp(App);
+
+app.use(VueFire, {
+    firebaseApp,
+});
+
+app.mount('#app');
