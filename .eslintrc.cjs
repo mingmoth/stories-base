74331841967c7a3154d70c48c11cc60996af4beb
Guanmin Liao <77824905+mingmoth@users.noreply.github.com>
2023-06-25 22:54:22 +0800

    [Eslint] 新增 Eslint 相關套件 (#2)
    * chore: add esline & eslint-plugin-vue
    - npx eslint --init
    
    * chore: add vite-plugin-eslint
    
    * chore: add vue/eslint-config-standard
    - add npm run lint script
    
    * feat: add eslintrc indent rules
    
    * chore: run lint fix

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/vue3-essential',
+        '@vue/standard'
+    ],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [
+                '.eslintrc.{js,cjs}'
+            ],
+            parserOptions: {
+                sourceType: 'script'
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: [
+        'vue'
+    ],
+    rules: {
+        indent: ['warn', 4],
+        'max-depth': ['warn', 4]
+    }
+}
