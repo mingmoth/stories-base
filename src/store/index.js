6db5c92eeaef600350bf4ae118ebe3afa04fb8b1
mingmoth <mingmoth@gmail.com>
2023-07-03 15:14:13 +0800

    feat: 新增 local store

---
+++ src/store/index.js
@@ -0,0 +1,74 @@
+import { computed, reactive, readonly } from 'vue'
+import { ajaxGetStoryIdList, ajaxGetStory } from '../api'
+
+// state
+const state = reactive({
+    storyIdList: [],
+    stories: [],
+    index: -1,
+    isLoading: true
+})
+
+export const mapState = readonly(state)
+
+// getter
+export const currentStory = computed(() => {
+    return state.stories[state.index] || {}
+})
+
+// action
+// 取得 StoryIdList
+export const getStoryIdList = async () => {
+    if (Array.isArray(state.storyIdList) &&
+        state.storyIdList.length) {
+        return
+    }
+    state.isLoading = true
+    try {
+        const response = await ajaxGetStoryIdList()
+        const { idList } = response
+        if (idList) {
+            state.storyIdList = idList.splice(0, 3)
+        }
+    } catch (error) {
+        console.error(error)
+    }
+    state.isLoading = false
+}
+
+// 取得 全部Stories
+export async function fetchStoriesData () {
+    if (Array.isArray(state.storyIdList) && state.storyIdList.length === 0) {
+        return
+    }
+    for (let index = 0; index < state.storyIdList.length; index++) {
+        try {
+            const storyId = state.storyIdList[index]
+            const story = await ajaxGetStory(storyId)
+            if (!story) {
+                throw new Error(`No Such ${storyId} Story`)
+            }
+            state.stories[index] = story
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    state.index = 0
+}
+
+export const nextStory = () => {
+    if (state.index + 1 === state.storyIdList.length) {
+        state.index = 0
+        return
+    }
+    state.index += 1
+    console.log('state.index', state.index)
+}
+
+export const prevStory = () => {
+    if (state.index === 0) {
+        return
+    }
+    state.index -= 1
+    console.log('state.index', state.index)
+}
