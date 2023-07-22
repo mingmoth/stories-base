78941026f8742e3429a95692a0b3c2750aa53c04
mingmoth <mingmoth@gmail.com>
2023-07-22 16:08:31 +0800

    feat: add AvatarImage component & put into the StoryImage

---
+++ src/components/AvatarImage.vue
@@ -0,0 +1,17 @@
+<template>
+    <img
+        src="../../public/avatar.png"
+        alt="avatar"
+        class="avatar"
+    >
+</template>
+
+<style lang="scss" scoped>
+.avatar {
+    position: absolute;
+    top: 16px;
+    left: 16px;
+    width: 48px;
+    height: 48px;
+}
+</style>
